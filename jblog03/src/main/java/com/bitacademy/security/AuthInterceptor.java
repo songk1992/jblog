5f04767e09df1a6214b90c4ebf28529ec440a3da
shk <shk@DESKTOP-C27PVRS>
2021-01-12 18:04:16 +0900

    블로그 관리자가 맞는지 체크 기능 추가

--- jblog03/src/main/java/com/bitacademy/security/AuthInterceptor.java
+++ jblog03/src/main/java/com/bitacademy/security/AuthInterceptor.java
@@ -1,10 +1,16 @@
 package com.bitacademy.security;
 
+import java.util.Map;
+import java.util.Optional;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.bitacademy.jblog.vo.UserVo;
@@ -50,6 +56,17 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 			return false;
 		}
 
+
+		// 사용자 명과 경로가 다를 경우 // TODO 올바른 방법으로 수정
+		@SuppressWarnings("unchecked")
+		final Map<String, String> variableValueMap = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+
+		if(!authUser.getId().equals(variableValueMap.get("id"))) {
+			return false;
+		}
+		
+		
+		
 		// 7. 권한 (Authorizaton) 체크를 위해서 @Auth의 role 가져오기 ("USER", "ADMIN")
 		String role = auth.value();
 		System.out.println(role);
@@ -58,6 +75,12 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 		if ("USER".equals(role)) {
 			return true;
 		}
+
+		
+		
+		
+		
+		
 
 		// 9. @Auth의 role이 "ADMIN"인 경우에는 authUser가 "ADMIN" 이어야 한다.
 //		if (!"ADMIN".equals(authUser.getRole())) {
@@ -72,4 +95,12 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 
 	}
 
+	public boolean hello(@PathVariable String id) {
+		boolean isOwner = false;
+		System.out.println(id);
+		System.out.println(id);
+		System.out.println(id);
+		System.out.println(id);
+		return isOwner;
+	}
 }
